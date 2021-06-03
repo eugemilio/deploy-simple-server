02cace06f76997904582ffd8f489365d1261c4b6
eugemilio <85132644+eugemilio@users.noreply.github.com>
2021-06-03 16:09:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req,res,next)=>{
-  res.send("This is my personal server...
-           
-                                               +              #####
-                                   / \
- _____        _____     __________/ o \/\_________      _________
-|o o o|_______|    |___|               | | # # #  |____|o o o o  | /\
-|o o o|  * * *|: ::|. .|               |o| # # #  |. . |o o o o  |//\\
-|o o o|* * *  |::  |. .| []  []  []  []|o| # # #  |. . |o o o o  |((|))
-|o o o|**  ** |:  :|. .| []  []  []    |o| # # #  |. . |o o o o  |((|))
-|_[]__|__[]___|_||_|__<|____________;;_|_|___/\___|_.|_|____[]___|  |")
+  res.send("This is my personal server...")
 })
 //heroku injects the port number into the PORT env value
 const port = process.env.PORT || 3001;
